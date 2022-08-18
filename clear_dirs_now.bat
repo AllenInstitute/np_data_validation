02aaef52ebfe21fd35b10343fdd6aeb0a40f5f53
bjhardcastle <ben.hardcastle@alleninstitute.org>
2022-08-18 16:28:16 -0700

    Fix bat file for env creation

--- clear_dirs_now.bat
+++ clear_dirs_now.bat
@@ -1,10 +1,6 @@
 @REM if necessary, clone repo with:
 @REM git clone https://github.com/AllenInstitute/np_data_validation.git
-git checkout main
-git pull 
 
-@REM if environment doesn't exist, create it with:
-@REM conda create --file environment.yml
 ECHO off
 title Checking for valid backups and clearing local directories
 
